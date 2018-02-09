ffa1f17a8c131fd8156431aa89892f0227ab3ed2
cormacio100 <cormac.music@gmail.com>
2018-02-09 18:32:16 +0000

    Deployment to HEROKU details

--- settings/staging.py
+++ settings/staging.py
@@ -1,12 +1,10 @@
 from base import *
+import dj_database_url
 
 DEBUG = False
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config('CLEARDB_DATABASE_URL')   #   CLEARDB_DATABASE_URL retrieved from Heroku App
 }
 
 SITE_URL = 'https://nightblind.herokuapp.com'

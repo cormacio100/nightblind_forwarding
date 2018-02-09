eacd45d7e2018bcc5ad7167ff0edf214de72cd54
cormacio100 <cormac.music@gmail.com>
2018-02-09 21:20:28 +0000

    GET, POST, PUT, DELETE functions added to API and URL updated

--- band_members/urls.py
+++ band_members/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from band_members.views import BandMemberView
 
 urlpatterns = [
-    url(r'^$',BandMemberView.as_view())
+    url(r'^$',BandMemberView.as_view()),
+    url(r'(?P<pk>[0-9]+)/$',BandMemberView.as_view())   #   url to pass values to the PUT method by <pk>
 ]
--- band_members/views.py
+++ band_members/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 from band_members.serializers import BandMemberSerializer
 from band_members.models import Band_Member
 
@@ -10,16 +11,96 @@ class BandMemberView(APIView):
         BandMemberView used to handle the incoming requests relating to
         `band_member` items
     """
-    def get(self,request):
+    def get(self,request,pk=None):
         """
         Retrieve a list of Band_Member items from the Band_Member items
         :param request:
         :return:
         """
-        band_member_items = Band_Member.objects.all()
-        # Serialize the data retrieved from the DB and serialize
-        # them using the `BandMemberSerializer`
-        serializer = BandMemberSerializer(band_member_items,many=True)
-        # Store the serialized data `serialized_data`
-        serialized_data = serializer.data
-        return Response(serialized_data)
+        if pk is None:
+            band_member_items = Band_Member.objects.all()
+            # Serialize the data retrieved from the DB and serialize
+            # them using the `BandMemberSerializer`
+            serializer = BandMemberSerializer(band_member_items,many=True)
+            # Store the serialized data `serialized_data`
+            serialized_data = serializer.data
+            return Response(serialized_data)
+        else:
+            band_member = Band_Member.objects.get(id=pk)
+            # Serialize the data retrieved from the DB and serialize
+            # them using the `BandMemberSerializer`
+            serializer = BandMemberSerializer(band_member)
+            # Store the serialized data `serialized_data`
+            serialized_data = serializer.data
+            return Response(serialized_data)
+
+
+    def post(self,request):
+        """
+        Handle POST requests for the 'band_members/' endpoint
+
+        This view will take the data property from the 'request' object, desrialize it into a Band_Member object,
+        and store it in the DB
+
+        Returns a 201 (successfully created) if the item is successfully created, otherwise returns a 400 (bad request)
+
+        :param request:
+        :return:
+        """
+        serializer = BandMemberSerializer(data=request.data)
+
+        #   Check to see if the data in the 'request' is valid
+        #   If not:
+        #   -   a bad request response is returns
+        #   Else:
+        #   -   save the data and return the data and a success response
+        if not serializer.is_valid():
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+        else:
+            serializer.save()
+            return Response(serializer.data,
+                            status=status.HTTP_201_CREATED)
+
+    def put(self,request,pk):
+        """
+        Handle PUT request for the 'band_members/' endpoint
+
+        Retrieves a 'band_member/' instance based on the primary key contained in the URL
+        Then takes the data property from the 'request' object to update the relevant 'band_member' instance
+
+        Returns the updated object if the update was successful,
+        otherwise
+        400 (bad request) is returned
+        :param request:
+        :param pk:
+        :return:
+        """
+        todo = Band_Member.objects.get(id=pk)
+        serializer = BandMemberSerializer(todo,data=request.data)
+
+        #   Check to see if the 'request' data is valid
+        #   If it cannot be deserialized into a Band_Member object then a bad request response will be returned containing the error
+        #   Else, save and return the data
+        if not serializer.is_valid():
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+        else:
+            serializer.save()
+            return Response(serializer.data)
+
+    def delete(self,request,pk):
+        """
+        Handle DELETE request for the 'band_member' endpoint
+
+        Retrieves a 'band_member' instance based on the pk contained in the URL and then deletes the relevant instance
+
+        Returns a 204 (no content) status to indicate that the item was deleted
+        :param request:
+        :param pk:
+        :return:
+        """
+        band_member = Band_Member.objects.get(id=pk)
+        band_member.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
